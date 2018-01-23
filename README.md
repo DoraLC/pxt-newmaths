fe7c1fe7923d8b3731f68dd1787286471c62fa20
DoraLC <34760377+DoraLC@users.noreply.github.com>
2018-01-23 11:50:01 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# pxt-newmaths
+# AdvMaths
+
+More maths blocks
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
+
+```package
+AdvMaths=github:DoraLC/pxt-newmaths
+```
---
+++ main.ts
@@ -0,0 +1,33 @@
+//% weight=0 color=#3CB371 icon="\uf0ad" block="Amaths"
+namespace Amaths {
+    //% blockId="placeValue" block="change place value number %int_value|base %base"
+    //% blockGap=2 weight=0 blockExternalInputs=true
+    export function make_base_number(int_value: number, base: number): string {
+        if (int_value == 0) {
+            return "0"
+        }
+        else {
+            let Number = int_value
+            let quotient = 0
+            let returnValue = ""
+            let on = ""
+            let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            let bit = 0
+            while (Number > 0) {
+                bit = Number % base
+                on = bit.toString()
+                if (base > 10) {
+                    for (let i = 10; i <= base - 1; i++) {
+                        if (i == bit) {
+                            on = alphabet[i - 10]
+                        }
+                    }
+                }
+                quotient = Number / base
+                returnValue = on + returnValue
+                Number = quotient
+            }
+            return returnValue
+        }
+    } 
+}
